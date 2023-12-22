657773bb9b7c2f071a83affc2fdd7a07a65e506b
Ace <AceQuantumBit@gmail.com>
2023-12-22 17:20:37 -0600

    oops

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -4,16 +4,16 @@
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    binary_tree_t *newnode = malloc(sizeof(binary_tree_t));
+	binary_tree_t *newnode = malloc(sizeof(binary_tree_t));
 
-    if (newnode == NULL)
-        return (0);
+	if (newnode == NULL)
+		return (0);
 
-    newnode->n = value;
-    newnode->parent = parent;
+	newnode->n = value;
+	newnode->parent = parent;
 
-    newnode->left = NULL;
-    newnode->right = NULL;
+	newnode->left = NULL;
+	newnode->right = NULL;
 
-    return (newnode);
+	return (newnode);
 }

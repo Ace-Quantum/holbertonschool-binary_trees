2a252f9b877ab37e84122d2ac75cb65027a1a8ff
Ace <AceQuantumBit@gmail.com>
2023-12-22 20:34:43 -0600

    this cat is driving me nuts

---
+++ 16-binary_tree_is_perfect.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "binary_trees.h"
+
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+    int sleft = 0;
+    int sright = 0;
+
+    if (!tree)
+        return (0);
+    
+    if (tree->left)
+        sleft = binary_tree_size(tree->left);
+    
+    if (tree->right)
+        sright = binary_tree_size(tree->right);
+    
+    if (size_l != size_r)
+        return (0);
+
+    return (1);
+}
